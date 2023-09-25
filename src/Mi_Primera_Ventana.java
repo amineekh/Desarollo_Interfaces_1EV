2dcb2d657fb1744b7a224505038a36116930108b
MohamedAmineElkhayya <amineelkhayyar8@gmail.com>
2023-09-25 09:27:53 +0200

    25 / 09 /2023

---
+++ src/Mi_Primera_Ventana.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Mi_Primera_Ventana {
+    public static void main(String[] args) {
+
+        JFrame f =  new JFrame();
+        f.setBounds(10,10,300,200);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
